be9076f622a52b287a2f221abf8ddbbc6f5788d0
Subhan <subhan.mps@gmail.com>
2016-09-09 04:21:39 +0500

    make it better

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express();
+var gm = require('gm')
+  , imageMagick = gm.subClass({ imageMagick: true });
+
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -15,6 +18,26 @@ app.get('/', function(request, response) {
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
+});
+
+app.get('/magic', function(req, res){
+    console.log("magic");
+    res.send("aaa");
+
+    // gm composite -gravity center change_image_url base_image_url
+
+gm()
+.command("composite") 
+.in("-gravity", "center")
+.in('/images/pic1.jpg')
+.in('/images/pic2.jpg')
+.write('/images/pic3.jpg', function (err) {
+  if (!err) 
+    console.log(' hooray! ');
+  else
+    console.log(err);
+});
+
 });
 
 
